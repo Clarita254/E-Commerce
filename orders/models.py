3b833c1389dcb1faa9687ff1609360dd8f92d96e
Clarita254 <clara.waithaka@strathmore.edu>
2024-11-19 12:17:05 +0300

    defined relationships in order model

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from .models import Customer
 
 # Create your models here.
+
+#customer-This is a foreign key referring to the customers model
+#order_date-This shows when the order is created
+#total_amount- stores the total amount from the order and is set to be a decimal field
 class Order(models.Model):
+    #One-Many relationship:Each order belongs to one customer
     customer=models.ForeignKey(
 
       Customer,
       on_delete=models.CASCADE, #Deletes orders if associated customer is deleted
-      related_name="orders" #Allows reverse lookup from Customers to Orders
+      related_name="orders" #allows to retrieve all orders for customer using customer.orders.all()
 
 
 
