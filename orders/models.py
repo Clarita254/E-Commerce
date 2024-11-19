e49254aceb293405238ad1765e2d24697281cce6
Clarita254 <clara.waithaka@strathmore.edu>
2024-11-19 11:41:32 +0300

    Created an Order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    customer=models.ForeignKey(
+
+      Customer,
+      on_delete=models.CASCADE, #Deletes orders if associated customer is deleted
+      related_name="orders" #Allows reverse lookup from Customers to Orders
+
+
+
+    )
+    order_date=models.DateTimeField(auto_now_add=True) #Automatically sets the date and time when order is created
+    total_amount=models.DecimalField(max_digits=10,decimal_places=2) #Stores the total order amount
+
+def __str__(self):
+   return f"Order #{self.id} by {self.customer.name}"
