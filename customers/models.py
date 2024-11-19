3dd94f5d3c01716e8694253cb6d8fa7c9743a128
Clarita254 <clara.waithaka@strathmore.edu>
2024-11-19 11:45:42 +0300

    Created a customer model

--- customers/models.py
+++ customers/models.py
@@ -3,4 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    name=models.Char
+    name=models.CharField(max_length=255) #Customer's name
+    email=models.EmailField(unique=True)  #Customer's email should be unique
+
+    def __str__(self):
+       return self.name
+    
