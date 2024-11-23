fca88a16a37c35d3caadc0eed38c5285ea8e1597
Clarita254 <clara.waithaka@strathmore.edu>
2024-11-23 12:11:16 +0300

    Created a customers and orders model

--- E_Commerce/models.py
+++ E_Commerce/models.py
@@ -20,3 +20,26 @@ class Customer(models.Model):#The customer model represents customer entity
 #It has a one-many relationship:Each order belongs to one Customer
 #The foreign key establishes a link between the order model and the customer model
 
+
+
+# Create your models here.
+class Order(models.Model):
+
+          customer = models.ForeignKey(
+              'Customer',
+          on_delete=models.CASCADE, #deletes orders if the associated customer is deleted
+          related_name='orders' #Enables accessing  all orders of a customer via customer.order.all()
+
+)
+
+ #Automatically  sets the date and time when an order is created         
+order_date=models.DateTimeField(auto_now_add=True)
+
+#Stores the total order amount with of 10 digits and 2 decimal places
+total_amount=models.DecimalField(max_digits=10, decimal_places=2)
+
+def __str__(self):
+  return f"Order #{self.id} by {self.customer.name}"
+ 
+ 
+
