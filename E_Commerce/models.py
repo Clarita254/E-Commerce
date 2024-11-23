3f14d5273c7c9a1edd25983e022bdbc41990222c
Clarita254 <clara.waithaka@strathmore.edu>
2024-11-23 13:29:58 +0300

    Created migrations for the customer and order model and tested its  functionality with sample data

--- E_Commerce/models.py
+++ E_Commerce/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 # Create your models here.
 
 
@@ -22,24 +20,21 @@ class Customer(models.Model):#The customer model represents customer entity
 
 
 
-# Create your models here.
+# Create your models here
+
 class Order(models.Model):
+    # ForeignKey linking to the Customer model
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,  # Delete orders if the associated customer is deleted
+        related_name='orders'  # Access orders through customer.orders.all()
+    )
 
-          customer = models.ForeignKey(
-              'Customer',
-          on_delete=models.CASCADE, #deletes orders if the associated customer is deleted
-          related_name='orders' #Enables accessing  all orders of a customer via customer.order.all()
+    # Automatically set the date and time when an order is created
+    order_date = models.DateTimeField(auto_now_add=True)
 
-)
+    # Store the total amount with 10 digits and 2 decimal places
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
- #Automatically  sets the date and time when an order is created         
-order_date=models.DateTimeField(auto_now_add=True)
-
-#Stores the total order amount with of 10 digits and 2 decimal places
-total_amount=models.DecimalField(max_digits=10, decimal_places=2)
-
-def __str__(self):
-  return f"Order #{self.id} by {self.customer.name}"
- 
- 
-
+    def __str__(self):
+         return f"Order #{self.id} by {self.customer.name}"
--- E_Commerce/settings.py
+++ E_Commerce/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'E_Commerce',
-    'customers',
-    'orders',
+    
     
 ]
 
